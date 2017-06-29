9f39bb3fcdba106c6a692034cc0b35fa77724723
Ed Mckain <mckain.default@gmail.com>
2017-06-29 10:21:12 +0200

    adds basic structure for the Currencylayer client

---
+++ lib/command_line_currency/currencylayer.rb
@@ -0,0 +1,14 @@
+require 'openssl'
+require 'base64'
+require 'yaml'
+
+module CommandLineCurrency
+  class Currencylayer
+    attr_reader :base_url, :api_key
+
+    def initialize
+      # TODO: Initialize the object.
+      # base_url ^ api_key
+    end
+  end
+end
